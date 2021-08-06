e4d3d206afc859bf40910cfbf71df1feb9fd9733
James <clementjaymes@.com>
2021-08-05 20:47:13 -0700

    adding initial files for the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import Board from './components/Board';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="flexbox">
+        <Board id="board-1" className="board">
+          <Card id="card-1" className="card" draggable="true">
+            <p>Card one</p>
+          </Card>
+          <Card id="card-1" className="card" draggable="true">
+            <p>Card one</p>
+          </Card>
+          <Card id="card-1" className="card" draggable="true">
+            <p>Card one</p>
+          </Card>
+          <Card id="card-1" className="card" draggable="true">
+            <p>Card one</p>
+          </Card>
+          <Card id="card-1" className="card" draggable="true">
+            <p>Card one</p>
+          </Card>
+          <Card id="card-1" className="card" draggable="true">
+            <p>Card one</p>
+          </Card>
+          <Card id="card-1" className="card" draggable="true">
+            <p>Card one</p>
+          </Card>
+          <Card id="card-1" className="card" draggable="true">
+            <p>Card one</p>
+          </Card>
+        </Board>
+
+        <Board id="board-2" className="board">
+          <Card id="card-2" className="card" draggable="true">
+            <p>Card Two</p>
+          </Card>
+        </Board>
+      </main>
     </div>
   );
 }
